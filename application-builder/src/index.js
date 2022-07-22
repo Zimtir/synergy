4da652f484b5b0612e42f28c6c732e3c80103ce5
Zimtir <zimtir@mail.ru>
2022-07-22 23:13:28 +0300

    feat: resolve show up of version

--- application-builder/src/index.js
+++ application-builder/src/index.js
@@ -1,3 +1,5 @@
+import packageJson from "../package.json";
+
 import { initApplication } from "../../application-source";
 
-initApplication();
+initApplication(packageJson.homepage, packageJson.version);
--- application-source/index.js
+++ application-source/index.js
@@ -1,9 +1,34 @@
-export const initApplication = () => {
+export const initApplication = (host, version) => {
   const application = document.getElementById("application");
   if (!application) {
     alert("no app box provided");
     return;
   }
 
-  application.innerHTML = "<h1>Hello world again</h1>";
+  const bundles = withBundles(host, version).map((bundle) => {
+    const link = document.createElement("a");
+
+    link.href = bundle.url;
+    link.innerText = bundle.name;
+
+    return link;
+  });
+
+  application.append(...bundles);
 };
+
+const withBundle = (host, name, version) => {
+  const url = `${host}/releases/download/v${version}/synergy-${name}`;
+
+  return {
+    url,
+    name,
+  };
+};
+
+export const withBundles = (host, version) => [
+  withBundle(host, "linux_arm64", version),
+  withBundle(host, "linux_x64", version),
+  withBundle(host, "mac_x64", version),
+  withBundle(host, "win_x64", version),
+];
